1cb8fff66d544f5150afb6ce45220dec0bbf623b
Jayphen <jayphen@gmail.com>
2017-11-14 17:33:40 +0100

    Add a warning for portrait gallery types

--- src/App.js
+++ src/App.js
@@ -300,6 +300,10 @@ class GalleryType extends Component {
     ))
   };
 
+  getTypeWarning = () => (
+    <div className="mt-3 text-xs text-red">Make sure you only use 1 portrait image for this gallery type, otherwise the second one will not display!</div>
+  )
+
   render() {
     return (
       <div className="mb-8">
@@ -317,6 +321,7 @@ class GalleryType extends Component {
             <svg className="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
           </div>
         </div>
+        {this.props.galleryType.indexOf('portrait') != -1 ? this.getTypeWarning() : ''}
       </div>
     )
   }
